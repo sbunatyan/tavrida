716d243b465f03809a1e0074147d178053ea1d29
Sergey Bunatyan <sbunatyan@mirantis.com>
2015-11-21 23:36:41 +0300

    Validation is moved to message from middleware

--- tavrida/messages.py
+++ tavrida/messages.py
@@ -1,7 +1,9 @@
 import anyjson
+import logging
 import uuid
 
 import entry_point
+import exceptions
 
 
 class AMQPMessage(object):
@@ -12,10 +14,39 @@ class AMQPMessage(object):
     This class is used in AMQP drivers to send AMQP Message to preprocessor
     """
 
+    REQUIRED_HEADERS = ["correlation_id", "message_id", "request_id",
+                        "message_type", "source", "destination"]
+    MESSAGE_TYPE = ["request", "response", "notification", "error"]
+
     def __init__(self, body, headers):
         super(AMQPMessage, self).__init__()
         self.body = body
         self.headers = headers
+        self.log = logging.getLogger(__name__)
+
+    def _validate_headers(self, headers):
+        for field in self.REQUIRED_HEADERS:
+            if headers.get(field) is None:
+                raise exceptions.FieldMustExist(field=field)
+        self._validate_message_type(headers["message_type"])
+        self._validate_message_type(headers["message_type"])
+        self._validate_entry_point(headers["source"], "source")
+        if headers["reply_to"]:
+            self._validate_entry_point(headers["reply_to"], "reply_to")
+        if headers["destination"]:
+            self._validate_entry_point(headers["destination"], "destination")
+
+    def _validate_message_type(self, message_type):
+        if message_type not in self.MESSAGE_TYPE:
+            raise exceptions.UnsuitableFieldValue(field="message_type",
+                                                  value=message_type)
+
+    def _validate_entry_point(self, value, name):
+        if not value:
+            raise exceptions.FieldMustFullyDefined(field=name)
+
+    def validate(self):
+        self._validate_headers(self.headers)
 
     @classmethod
     def create_from_message(cls, message):
--- tavrida/steps.py
+++ tavrida/steps.py
@@ -1,42 +1,11 @@
-import logging
-
 import controller
-import exceptions
 import messages
 
 
 class ValidateMessageMiddleware(controller.AbstractController):
 
-    REQUIRED_HEADERS = ["correlation_id", "message_id", "request_id",
-                        "message_type", "source", "destination"]
-    MESSAGE_TYPE = ["request", "response", "notification", "error"]
-
-    def __init__(self):
-        self.log = logging.getLogger(__name__)
-
-    def _validate_headers(self, headers):
-        for field in self.REQUIRED_HEADERS:
-            if headers.get(field) is None:
-                raise exceptions.FieldMustExist(field=field)
-        self._validate_message_type(headers["message_type"])
-        self._validate_message_type(headers["message_type"])
-        self._validate_entry_point(headers["source"], "source")
-        if headers["reply_to"]:
-            self._validate_entry_point(headers["reply_to"], "reply_to")
-        if headers["destination"]:
-            self._validate_entry_point(headers["destination"], "destination")
-
-    def _validate_message_type(self, message_type):
-        if message_type not in self.MESSAGE_TYPE:
-            raise exceptions.UnsuitableFieldValue(field="message_type",
-                                                  value=message_type)
-
-    def _validate_entry_point(self, value, name):
-        if not value:
-            raise exceptions.FieldMustFullyDefined(field=name)
-
     def process(self, ampq_message):
-        self._validate_headers(ampq_message.headers)
+        ampq_message.validate()
         return ampq_message
 
 
