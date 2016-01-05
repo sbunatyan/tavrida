662d4005911c7c8f278b81819ee8ba99c1b53479
Sergey Bunatyan <sbunatyan@mirantis.com>
2016-01-05 17:59:51 +0300

    Docstrings for steps

--- tavrida/steps.py
+++ tavrida/steps.py
@@ -3,6 +3,9 @@ import messages
 
 
 class ValidateMessageMiddleware(controller.AbstractController):
+    """
+    Validates message headers
+    """
 
     def process(self, ampq_message):
         ampq_message.validate()
@@ -10,12 +13,18 @@ class ValidateMessageMiddleware(controller.AbstractController):
 
 
 class CreateMessageMiddleware(controller.AbstractController):
+    """
+    Creates message from raw RabbitMQ message
+    """
 
     def process(self, message_body):
         return messages.IncomingMessageFactory().create(message_body)
 
 
 class CreateAMQPMiddleware(controller.AbstractController):
+    """
+    Creates intermediate AMQP message
+    """
 
     def process(self, message):
         return messages.AMQPMessage.create_from_message(message)
